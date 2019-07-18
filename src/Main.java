83aa0f7d31965239e294df772c0c4ada62cbc194
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-18 18:34:24 +0000

    Konto Aufgabe

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    
+    /**
+     * Kontosystem
+     */
+    public static void main(String[] args){
+        // Legen Sie eine Klasse Konto an, die mit einem Betrag und 
+        // Kontoinhaber initialisiert werden kann. Die Klasse Konto 
+        // soll zudem die Anzahl getätigter Transaktionen speichern 
+        // und alle Attribute durch geeignete Methoden ändern bzw. 
+        // ausgeben lassen.
+        // Erinnerung: Denken Sie an Kapselung!
+        
+    }
+    
+}
