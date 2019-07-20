96fd1a14d2ad55e51ccdd3d2f12eaee248130005
Arakai-dev <rose.patrick@gmx.de>
2019-07-20 10:09:42 +0000

    Passt

---
+++ src/Konto.java
@@ -0,0 +1,33 @@
+public class Konto {
+    private double betrag;
+    private String inhaber;
+    private int anzahl;
+
+    public double getBetrag() {
+        return betrag;
+    }
+
+    public void setBetrag(double value) {
+        betrag = value;
+    }
+
+    public String getInhaber() {
+        return inhaber;
+    }
+
+    public void setInhaber(String value) {
+        inhaber = value;
+    }
+
+    public int getAnzahl() {
+        return anzahl;
+    }
+
+    public void setAnzahl(int value) {
+        anzahl = value;
+    }
+    
+    public void countAnzahl(){
+        anzahl++;
+    }
+}
